7b1bd4d94fce1ad51e740ec475b8c939b84812ea
sumangoyal1021 <sumangoyal1021@gmail.com>
2020-10-10 22:38:22 +0530

    suman

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAorwFms4cWXhmUjgVw7BczpG8n-BnYr3E",
+    authDomain: "qcode-45886.firebaseapp.com",
+    databaseURL: "https://qcode-45886.firebaseio.com",
+    projectId: "qcode-45886",
+    storageBucket: "qcode-45886.appspot.com",
+    messagingSenderId: "278152110295",
+    appId: "1:278152110295:web:6fc99954946d300755d092",
+    measurementId: "G-8XGK7N4YFS"
+};
+firebase.initializeApp(firebaseConfig);
